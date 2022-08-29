82d9695909bb7773a78cdaa5721d0d044fc6d867
benaiah-ke <benaiahpraise@gmail.com>
2022-08-29 08:45:58 +0300

    Done with lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById('create-task-form').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    // Our input element
+    var input = document.getElementById('new-task-description');
+
+    // Get the new task from the input
+    var task = input.value;
+
+    // Add to list
+    document.getElementById('tasks').innerHTML += `
+      <li>${task}</li>
+    `;
+
+    // Clear the input
+    input.value = '';
+  });
 });
